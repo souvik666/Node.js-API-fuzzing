a64caab36c8f022f6c0c3640acefc56b450563a3
souvik666 <peacedynasty0@gmail.com>
2021-11-06 10:57:08 +0530

    Node.js-API-fuzzing

---
+++ Fuzzer.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const axios = require("axios").default;
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var logger = fs.createWriteStream("log.txt", {
+  flags: "a", //[...]
+});
+
+const FetchStatsu = (url, paths) => {
+  let i = 0;
+
+  axios
+    .get(url + `/${paths}`)
+    .then(function (response) {
+      if (response.status !== 404) {
+        logger.write(`${url}/${paths} status--> ${response.status}\n`);
+        console.log(`Found one with status ${response.status} count:${++i}`);
+      }
+    })
+    .catch(function (error) {
+      console.log("still searching...");
+    })
+    .then(function () {});
+};
+
+const wordlist = (url) => {
+  fs.readFile("World.list.txt", "utf8", function (err, data) {
+    data = data.trim().split("\n");
+    for (let i of data) {
+      FetchStatsu(url, i);
+    }
+  });
+};
+
+readline.question("Target Url" + "\n", (url) => {
+  if (!url || !url.includes("https" || "http") || url[url.length - 1] !== "/")
+    return console.log(
+      "No url specified or url doenst contain http or https or last index got a /"
+    );
+
+  wordlist(url);
+  readline.close();
+});
+
+/* FetchStatsu("https://duttatyreaapi.herokuapp.com", "bike");
+ */
